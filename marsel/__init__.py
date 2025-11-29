edc4b4a7f691a5a6ed969ef21ab355694eab20b7
Shaxzodbek <muxtorovshaxzodbek16@gmail.com>
2025-11-30 02:56:28 +0500

    Update __init__.py and pyproject.toml with author information and license format

--- marsel/__init__.py
+++ marsel/__init__.py
@@ -1,5 +1,10 @@
+"""
+Marsel - A lightweight utility library
+"""
 from .core import Marsel
 
 __version__ = "0.0.1"
+__author__ = "Shaxzodbek"
+__email__ = "muxtorovshaxzodbek16@gmail.com"
 
 __all__ = ["Marsel"]
